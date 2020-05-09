9740484b8069b795e646dd6387bf650cc9d29066
Roman Svitkha <griind@gmail.com>
2020-05-08 21:35:46 -0500

    Added homeController, couple views. Added basic authentication.

---
+++ Basic/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Basic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+        public IActionResult Authenticate()
+        {
+            var mainClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Vasa"),
+                new Claim(ClaimTypes.Email, "Vasa@gmail.com"),
+                new Claim("SomeKey", "SomeValue"),
+            };
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Vasa Pupkin"),
+                new Claim("DrivingRecord", "A+")
+        };
+            var mainIdentity = new ClaimsIdentity(mainClaims, "someIdentity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "licenseIdentity");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { mainIdentity, licenseIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+    
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Basic/Startup.cs
+++ Basic/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +11,14 @@ namespace Basic
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth", config =>
+                {
+                    config.Cookie.Name = "MyCookie";
+                    config.LoginPath = "/Home/Authenticate";
+                });
+
             services.AddControllersWithViews();
         }
 
@@ -26,8 +29,11 @@ namespace Basic
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
